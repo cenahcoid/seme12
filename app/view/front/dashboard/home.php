07bd05d95c97e796f7b0ae7539acdeea54375238
RUli Royana Widura <ruliwidura1>
2023-09-25 14:45:50 +0700

    Membuat halaman detail cerita hero Kimmy

--- app/view/front/dashboard/home.php
+++ app/view/front/dashboard/home.php
@@ -157,10 +157,10 @@
           <div class="card shadow-sm">
             <img src="media/kimmy2.png" alt="Italian Trulli">
             <div class="card-body">
-              <p class="card-text text-center"><b>Cerita Tentang Gunung Sinabung.</b></p>
+              <p class="card-text text-center"><b>Cerita Tentang Hero Kimmy</b></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <a href="#" ><button type="button" class="btn btn-primary btn-outline-secondary" style="color:#ffffff;">View</button></a>
+                  <a href="kimmy" ><button type="button" class="btn btn-primary btn-outline-secondary" style="color:#ffffff;">View</button></a>
                 </div>
                 <small class="text-muted">11 mins</small>
               </div>
