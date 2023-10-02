68832d8535ea6c2e6429216123e8dcc2e571c85b
RUli Royana Widura <ruliroyanawidura@gmail.com>
2023-10-02 17:00:46 +0700

    ubah font

--- app/view/front/home/home.php
+++ app/view/front/home/home.php
@@ -1,13 +1,11 @@
-
 <div class="row" style="background-image: url('media/background.png'); margin:0px;">
   <div class="col-md-12 text-center">
     <h1 class="text-center"></h1>
     <img src="media/bunga.png" alt="Italian Trulli">
     <img src="media/pengantin2.png" alt="Italian Trulli">
     <br />
-    <br />
     <h3 style="color:#a8475e;">Kepada Bpk/Ibu/Saudara/i</h3>
-    <p style="color:#a8475e; font-family:amaranth; font-size:13px;">Tanpa mengurangi rasa hormat, kami mengundang anda untuk hadir di acara pernikahan kami.</p>
+    <p style="color:#a8475e; font-family:amaranth; font-size:14px;">Tanpa mengurangi rasa hormat, kami mengundang anda untuk hadir di acara pernikahan kami.</p>
     <div class="text-center">
       <a href="awal"><button class="button button1 " style="background-color:#a8475e; border: 1px solid #a8475e; border-radius: 25px; color:#ffffff;"><b>Open Invitation</b></button></a>
     </div>
